8c4d547ae153795ba181eec269b149eacbc1eced
Rodrigo Giaffredo <rodrigo.giaffredo@gmail.com>
2026-08-17 11:52:49 -0300

    Commit atividade criação de variáveis
    Atividade em aula prof. Giuliana Bezerra sobre variáveis

---
+++ Aulas_Particulares/Gb02Variaveis.java
@@ -0,0 +1,24 @@
+
+// Prof. Giuliana Bezerra
+// Aula 02 - Variáveis
+
+
+public class Gb02Variaveis {
+
+    public static void main(String[] args) {
+        
+        /** 
+         * Sintaxe de declaração de varíavel em Java: 
+         * com valor: <tipo> identificador = valor;
+         * sem valor: <tipo: identificador;
+         * 
+         * Variável declarada LOCALMENTE dentro de bloco de comandos da função 'main'.
+         */
+        String saudacao = "Como vai você?";
+
+        /**
+         * Chamada da variável: é feita através de seu identificador
+         */
+        System.out.println("Olá! " + saudacao);
+    }    
+}
